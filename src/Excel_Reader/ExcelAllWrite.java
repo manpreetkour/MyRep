e853bb604efd7aea2a9083797135f0fbe1e10900
manpreetkour <manpreetkour@yahoo.com>
2016-02-15 08:47:05 -0500

    Junit Tests

--- src/Excel_Reader/ExcelAllWrite.java
+++ src/Excel_Reader/ExcelAllWrite.java
@@ -1,15 +1,19 @@
 package Excel_Reader;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.junit.Test;
 
 public class ExcelAllWrite {
-	public static void main(String[] args) throws IOException, InvalidFormatException {
+	@Test
+	public void allWrite() throws IOException{
+		
 		System.out.println("Writing into Excel files ..... ");
 		String path=System.getProperty("user.dir")+"\\src\\XLS_Files\\TestData.xlsx";
 		FileInputStream fis =new FileInputStream(path);
